e725acf9adbb6a1c8477b5a8cadc5c3dd6d9f9ff
Jhonierk <jorge.otalvaro00@usc.edu.co>
2021-03-08 14:30:57 -0500

    agregar lista de registros

--- refugio/apps/mascota/urls.py
+++ refugio/apps/mascota/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from apps.mascota.views import index, mascota_form
+from apps.mascota.views import index, mascota_form, mascota_list
 
 urlpatterns = [
     path('', index, name='index'),
     path('nuevo', mascota_form, name='mascota_crear'),
+    path('listar', mascota_list, name='mascota_listar'),
 ]
